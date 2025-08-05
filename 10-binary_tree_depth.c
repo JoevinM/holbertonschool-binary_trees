799785fb9b5ae21cfb30beb245d2ec32038915f3
joevinM <joevin.manceau@hotmail.fr>
2025-08-05 11:03:57 +0200

    Fix the output of Depth Task

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,33 +1,25 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 
+
 /**
  * binary_tree_depth - Measures the depth of a node in a binary tree
  * @tree: Pointer to the node to measure the depth
  *
- * Return: Depth of the node. If tree is NULL, return 0.
+ * Return: The depth of the node
+ *         0 if tree is NULL or the node is the root
  *
  * Description: The depth is the number of edges from the node
- * to the root node.
+ * to the root node. It is computed recursively by following
+ * the parent pointers up to the root.
  */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t compteur = 0;
-
 	if (tree == NULL)
 		return (0);
-
-	if (tree->left == NULL)
-	{
-		binary_tree_depth(tree->right);
-		compteur++;
-	}
-	if (tree->right == NULL)
-	{
-		binary_tree_depth(tree->left);
-		compteur++;
-	}
-	return (compteur);
-
+	if (tree->parent == NULL)
+		return (0);
+	else
+		return (1 + binary_tree_depth(tree->parent));
 }
